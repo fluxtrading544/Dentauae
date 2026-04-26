8929b6ff19434eec15f416c19b22da814b8ca01c
Shaheryar Ahmed <shaheryar.guru@outlook.com>
2026-04-27 04:04:37 +0500

    fix: changed admin path to /admin and added build debugging

--- medusa-dentalanti/medusa-config.ts
+++ medusa-dentalanti/medusa-config.ts
@@ -60,7 +60,7 @@ module.exports = defineConfig({
   ],
   admin: {
     disable: false,
-    path: "/app",
+    path: "/admin",
     backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
   },
 })
