55a2f647752d74dfa2c0f9b2ba68ea879031b79b
Shaheryar Ahmed <shaheryar.guru@outlook.com>
2026-04-28 00:41:51 +0500

    fix: disable cookieSecure for HTTP connection

--- medusa-dentalanti/medusa-config.ts
+++ medusa-dentalanti/medusa-config.ts
@@ -20,6 +20,7 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      cookieSecure: false,
     }
   },
   modules: [
